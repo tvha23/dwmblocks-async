3aa73ffe80c9d04b5bc63fad1484f2cf22e53201
tvha <ramazan.plt26@gmail.com>
2022-07-05 19:01:26 +0600

    init commit

---
+++ config.h
@@ -0,0 +1,12 @@
+#define CMDLENGTH 60
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+        BLOCK("~/.config/dwmblocks-async/scripts/curr_song",      0,    3),
+        BLOCK("~/.config/dwmblocks-async/scripts/volume",         0,    1),
+        BLOCK("~/.config/dwmblocks-async/scripts/cpu_temp",      10,    0),
+        BLOCK("~/.config/dwmblocks-async/scripts/dt",           300,    0),
+        BLOCK("~/.config/dwmblocks-async/scripts/clock",          1,    0),
+        BLOCK("~/.config/dwmblocks-async/scripts/battery",      120,    2),
+};
